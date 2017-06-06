d4b1bb6f552f741408655bb2a3f006c9b109c956
NawfalTachfine <nawfal.tachfine@aramisauto.com>
2017-06-06 13:00:30 +0200

    added app skeleton

---
+++ application.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+# Standard Libs
+from flask import Flask
+from flask import request
+# from flask import render_template
+from flask import jsonify
+
+import os
+import sys
+import numpy as np
+import pandas as pd
+import sklearn
+import json
+import requests
+import logging as lg
+import pickle as pkl
+from datetime import datetime as dt
+from pytz import timezone
+
+# Custom Libs
+sys.path.append("./lib")
+sys.path.append("./models")
+
+# ******************************************************************************
+
+# Conf
+current_time = dt.now(timezone('UTC')).astimezone(timezone('Europe/Paris'))
+log_file_name = 'logs/run.{}.log'.format(current_time.strftime("%Y-%m-%d"))
+
+lg.basicConfig(filename = log_file_name,
+               level = lg.INFO,
+               filemode = 'a',
+               format = '%(asctime)s\t%(levelname)s\t%(message)s',
+               datefmt = '%Y-%m-%d %H:%M:%S'
+               )
+
+# ******************************************************************************
+
+# Init
+
+app = Flask(__name__)
+
+# Core
+@app.route('/api/v1.0/aballone', methods=['POST'])
+def index():
+
+    out = {"key":"value"}
+
+    return jsonify(out)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
